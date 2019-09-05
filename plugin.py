f28cec7f2f428afea19998f890e7039e2eb76261
herr kaste <herr.kaste@gmail.com>
2019-09-05 19:50:51 +0200

    Let black handle the string quotes

--- plugin.py
+++ plugin.py
@@ -38,8 +38,8 @@ class AutomaticallyOpenFolderAsProject(sublime_plugin.EventListener):
             return
 
         KNOWN_WINDOWS.add(wid)
-        window.run_command('create_std_project_file')
-        window.run_command('open_the_project_instead')
+        window.run_command("create_std_project_file")
+        window.run_command("open_the_project_instead")
 
 
 class create_std_project_file(sublime_plugin.WindowCommand):
@@ -53,7 +53,7 @@ class create_std_project_file(sublime_plugin.WindowCommand):
         folder = window.folders()[0]
         dirname = os.path.basename(folder)
 
-        basename = dirname + '.sublime-project'
+        basename = dirname + ".sublime-project"
         project_file_name = os.path.join(folder, basename)
         if os.path.exists(project_file_name):
             window.status_message(
@@ -61,19 +61,19 @@ class create_std_project_file(sublime_plugin.WindowCommand):
             )
             return
 
-        items = ['Create project file {!r}'.format(basename), 'No, thanks']
+        items = ["Create project file {!r}".format(basename), "No, thanks"]
 
         def on_done(result: int) -> None:
             if result != 0:
                 return  # 'No' or cancelled
 
-            with open(project_file_name, 'w') as file:
+            with open(project_file_name, "w") as file:
                 file.write(PROJECT_TEMPLATE)
 
             window.status_message(
-                'Created project file `{}`'.format(project_file_name)
+                "Created project file `{}`".format(project_file_name)
             )
-            window.run_command('open_the_project_instead')
+            window.run_command("open_the_project_instead")
 
         window.show_quick_panel(items, on_done, sublime.KEEP_OPEN_ON_FOCUS_LOST)
 
@@ -87,20 +87,20 @@ class open_the_project_instead(sublime_plugin.WindowCommand):
         window = self.window
 
         folder = window.folders()[0]
-        pattern = os.path.join(folder, '*.sublime-project')
+        pattern = os.path.join(folder, "*.sublime-project")
         paths = glob(pattern)
         if not paths:
-            window.status_message('No project file in first folder')
+            window.status_message("No project file in first folder")
             return
 
         if len(paths) > 1:
-            window.status_message('More that one project file.')
+            window.status_message("More that one project file.")
             return
 
         open_wids = get_open_wids()
         bin = get_executable()
         path = paths[0]
-        cmd = [bin, '-p', path]
+        cmd = [bin, "-p", path]
         try:
             subprocess.Popen(cmd, startupinfo=create_startupinfo())
         except OSError:
@@ -122,7 +122,7 @@ def close_window(wid, open_wids):
 
     if current_wids != open_wids:
         window = sublime.Window(wid)
-        window.run_command('close_window')
+        window.run_command("close_window")
 
     sublime.set_timeout(lambda: close_window(wid, open_wids))
 
@@ -131,15 +131,15 @@ def close_window(wid, open_wids):
 # Copyright (c) 2017 Randy Lai <randy.cs.lai@gmail.com>
 def get_executable() -> str:
     executable_path = sublime.executable_path()
-    if sublime.platform() == 'osx':
-        app_path = executable_path[: executable_path.rfind('.app/') + 5]
-        executable_path = app_path + 'Contents/SharedSupport/bin/subl'
+    if sublime.platform() == "osx":
+        app_path = executable_path[: executable_path.rfind(".app/") + 5]
+        executable_path = app_path + "Contents/SharedSupport/bin/subl"
 
     return executable_path
 
 
-def create_startupinfo() -> 'Optional[subprocess.STARTUPINFO]':
-    if os.name == 'nt':
+def create_startupinfo() -> "Optional[subprocess.STARTUPINFO]":
+    if os.name == "nt":
         info = subprocess.STARTUPINFO()
         info.dwFlags |= subprocess.STARTF_USESHOWWINDOW
         return info
