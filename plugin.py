72d07b90446c8bac86a1cda7f4da2627dd744807
herr kaste <herr.kaste@gmail.com>
2019-10-02 12:32:14 +0200

    Implement open project command

--- plugin.py
+++ plugin.py
@@ -1,3 +1,4 @@
+from collections import deque
 from glob import glob
 import os
 import subprocess
@@ -6,11 +7,12 @@ import sublime
 import sublime_plugin
 
 try:
-    from typing import Optional, Set  # noqa
+    from typing import Deque, List, Optional, Set  # noqa
 except ImportError:
     ...
 
 
+STORAGE_FILE = "LastUsedProjects"
 KNOWN_WINDOWS = set()  # type: Set[sublime.WindowId]
 PROJECT_TEMPLATE = """
 {
@@ -97,25 +99,33 @@ class open_the_project_instead(sublime_plugin.WindowCommand):
             window.status_message("More that one project file.")
             return
 
+        window.run_command(
+            "open_project_in_new_window", {"project_file": paths[0]}
+        )
+
+
+class open_project_in_new_window(sublime_plugin.WindowCommand):
+    def run(
+        self, project_file: str, close_current: bool = True
+    ) -> None:  # type: ignore
+        window = self.window
         open_wids = get_open_wids()
+
         bin = get_executable()
-        path = paths[0]
-        cmd = [bin, "-p", path]
-        try:
-            subprocess.Popen(cmd, startupinfo=create_startupinfo())
-        except OSError:
-            raise
-        else:
+        cmd = [bin, "-p", project_file]
+        subprocess.Popen(cmd, startupinfo=create_startupinfo())
+
+        if close_current:
             sublime.set_timeout(lambda: close_window(window.id(), open_wids))
 
 
-def get_open_wids():
-    # type: () -> Set[sublime.WindowId]
+def get_open_wids() -> "Set[sublime.WindowId]":
     return {w.id() for w in sublime.windows()}
 
 
-def close_window(wid, open_wids):
-    # type: (sublime.WindowId, Set[sublime.WindowId]) -> None
+def close_window(
+    wid: "sublime.WindowId", open_wids: "Set[sublime.WindowId]"
+) -> None:
     current_wids = get_open_wids()
     if wid not in current_wids:
         return
@@ -145,3 +155,68 @@ def create_startupinfo() -> "Optional[subprocess.STARTUPINFO]":
         return info
 
     return None
+
+
+def get_paths_history() -> "List[str]":
+    s = sublime.load_settings(STORAGE_FILE)
+    return s.get("paths") or []
+
+
+def persist_paths_history(paths) -> None:
+    s = sublime.load_settings(STORAGE_FILE)
+    s.set("paths", paths[-30:])
+    sublime.save_settings(STORAGE_FILE)
+
+
+class RememberLastUsedProjects(sublime_plugin.EventListener):
+    def on_activated_async(self, view: sublime.View) -> None:
+        window = view.window()
+        if not window:
+            return
+
+        project_path = window.project_file_name()
+        if not project_path:
+            return
+
+        paths = get_paths_history()
+        if project_path not in paths:
+            paths.append(project_path)
+        elif paths[-1:] == [project_path]:
+            return
+        else:
+            paths.remove(project_path)
+            paths.append(project_path)
+
+        persist_paths_history(paths)
+        print("--> last_used_projects", [os.path.basename(p) for p in paths])
+
+
+class open_last_used_project(sublime_plugin.WindowCommand):
+    def run(self, project_file: str) -> None:
+        for w in sublime.windows():
+            if w.project_file_name() == project_file:
+                ag, av = w.active_group(), w.active_view()
+                w.focus_group(ag)
+                if av:
+                    w.focus_view(av)
+                return
+        else:
+            self.window.run_command(
+                "open_project_in_new_window",
+                {"project_file": project_file, "close_current": False},
+            )
+
+    def input(self, args):
+        if "project_file" not in args:
+            return ChooseProjectFile()
+
+
+class ChooseProjectFile(sublime_plugin.ListInputHandler):
+    def name(self) -> str:
+        return "project_file"
+
+    def list_items(self):
+        return [
+            (os.path.basename(p)[:-16], p)
+            for p in reversed(get_paths_history()[:-1])
+        ] or ["No projects in history."]
